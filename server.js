1b4a9d50585f9d767c9019bab82cd66d4cbb2e25
Nick Boutin <nboutin109@gmail.com>
2021-03-01 18:52:35 -0500

    heroku deployment

--- server.js
+++ server.js
@@ -15,13 +15,20 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/transaction", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://Thisuser:Transaction1@cluster0.d2how.mongodb.net/Transactions?retryWrites=true&w=majority", {
   useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true
 });
 
+
 app.use(require("./routes/api.js"));
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  console.log("open!")
+});
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
